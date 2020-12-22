fc47e25fee058e93dcc6a48f7baf1b6547a53160
João Paulo <joaopaulozangrande@gmail.com>
2020-12-22 15:03:36 -0300

    I improve create table.

--- conexao.php
+++ conexao.php
@@ -17,14 +17,14 @@ exit();
 $pdo->exec('
     CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY auto_increment,
-        name TEXT,
-         birth_date TEXT
+        name VARCHAR(128),
+         birth_date DATE
     );
     
     CREATE TABLE IF NOT EXISTS phones (
         id INTEGER PRIMARY KEY auto_increment ,
-        area_code TEXT,
-        number TEXT,
+        area_code CHAR(2),
+        number CHAR(9),
         student_id INTEGER,
         FOREIGN KEY (student_id) REFERENCES students(id)
     );
